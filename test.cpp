186a2ccd7026af1927e143f200fd9c9aa9f67cf7
Anna Khachatryan <79268348+crypto-annihilator@users.noreply.github.com>
2021-08-26 13:59:46 +0400

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+class name
+{
+public:
+    name(int x2) : x1 {x2}{}
+private:
+    int name;
+};
+
+int foo_awd_A_sdc(int xqsq, int, int);
+
+int main()
+{
+    string str = "1234567890";
+    unsigned int asa_ = 23;
+    int y = 0;
+    y += asa_;
+    ann aewf(y);
+}
+
+int foo_awd_A_sdc(int xq_fq, int fq_f, int fq)
+{
+    xq_fq = fq_f;
+    fq = xq_fq;
+    cout << "qefbqe" << xq_fq << endl;
+    return fq_f;
+}
